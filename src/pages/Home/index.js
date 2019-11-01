eace22ea0e9ae1b90991aa3194e1f0a278056743
unknown <douglassalvagni@gmail.com>
2019-11-01 11:17:44 -0300

    rotas scss

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                Página Home
+            </React.Fragment>
+        );
+    }
+}
+
+export default Home;
